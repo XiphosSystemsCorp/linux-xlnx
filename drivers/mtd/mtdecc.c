767100bdf51d235d6e904632ffd21af4a48a637f
Joshua Lamorie <joshua.lamorie@xiphos.com>
2014-11-18 23:58:47 -0500

    Added BCH encode and decode to MTD write and read respectively
    Signed-off-by: Joshua Lamorie <joshua.lamorie@xiphos.com>

--- drivers/mtd/mtdecc.c
+++ drivers/mtd/mtdecc.c
@@ -19,10 +19,17 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/mtd/mtd.h>
+#include <linux/bch.h>
 
 static struct mtd_info * mtd_under;
 
 static struct mtd_info * mtd_ecc;
+
+static struct bch_control * mtd_ecc_bch;
+
+static unsigned char * mtd_ecc_eccmask;
+
+static unsigned int * mtd_ecc_errloc;
 
 //static const char * mtd_ecc_name = "MTD_ECC";
 
@@ -105,6 +112,7 @@ static int mtd_ecc_read(struct mtd_info *mtd, loff_t from, size_t len,
 		size_t buf_cpy_len = 0;
 		loff_t under_from;
 		size_t under_retlen = 0;
+    int j = 0;
 
 		under_from = (start_sect + i) * 0x10000;
 		DBG_MTDECC(&mtd->dev,"under->_read : from: %llx\n",under_from);
@@ -120,12 +128,41 @@ static int mtd_ecc_read(struct mtd_info *mtd, loff_t from, size_t len,
 			kfree(mtd_ecc_buf);
 			return -EINVAL;
 		}
-#if 0
-		if ((start_offset + len) > 0x10000) {
-			dev_err(&mtd->dev,"Trying read beyond input buffer. %lld + %d\n",start_offset,len);
-			return -EINVAL;
+
+		// correct data
+		for (j=0;j<16;j++) {
+			int count;
+			unsigned char * read_ecc = &mtd_ecc_buf[0xf000 + (j*0x100)];
+      unsigned char * page = &mtd_ecc_buf[j * 0xf00];
+      int k;
+			
+      unsigned char calc_ecc[0x100];
+
+      memset(calc_ecc,0,sizeof(calc_ecc));
+      encode_bch(mtd_ecc_bch,page,0xf00,calc_ecc);
+
+
+      /* apply mask so that an erased page is a valid codeword */
+      for (k=0;k<0xff;k++) {
+        calc_ecc[k] ^= mtd_ecc_eccmask[k];
+      }
+
+			count = decode_bch(mtd_ecc_bch,page,0xf00,read_ecc,calc_ecc,NULL,mtd_ecc_errloc); 
+
+      if (count > 0) {
+        for (k=0;k<count;k++) {
+          if (mtd_ecc_errloc[k] < (0xf00*8) ) {
+            // error is in data, correct it
+            page[mtd_ecc_errloc[k] >> 3] ^= (1 << (mtd_ecc_errloc[k] & 7));
+          }
+          dev_notice(&mtd->dev,"%s: corrected bitflip %u\n",__func__,mtd_ecc_errloc[k]);
+        }
+      } else if (count < 0) {
+        dev_err(&mtd->dev,"ECC unrecoverable error (count:%d)\n",count);
+      }
+
 		}
-#endif
+
 
 		buf_out_index = i * 0xf000;
 		if (i == 0) {
@@ -188,7 +225,7 @@ static int mtd_ecc_write(struct mtd_info *mtd, loff_t to, size_t len,
 		loff_t under_to;
 		size_t under_retlen = 0;
 
-
+    int j;
 
 
 		buf_in_index = i * 0xf000;
@@ -218,6 +255,24 @@ static int mtd_ecc_write(struct mtd_info *mtd, loff_t to, size_t len,
 
 		// get the data from the requested offset in the buffer
 		memcpy(&mtd_ecc_buf[buf_out_index],&buf[buf_in_index],buf_cpy_len);
+
+    // encode the ecc stuff
+    for (j=0;j<16;j++) {
+      int k;
+      unsigned char * code = &mtd_ecc_buf[0xf000 + (j*0x100)];
+      unsigned char * page = &mtd_ecc_buf[j*0xf00];
+      
+
+      // Clear parity bits to zero
+      memset(code,0,0xff);
+      encode_bch(mtd_ecc_bch,page,0xf00,code);
+
+      /* apply mask so that an erased page is a valid codeword */
+      for (k=0;k<0xff;k++) {
+        code[k] ^= mtd_ecc_eccmask[k];
+      }
+    }
+
 
 		DBG_MTDECC(&mtd->dev,"under->_write: to 0x%llx\n",under_to);
 		if ((rc = mtd_under->_write(mtd_under,under_to,0x10000,&under_retlen,mtd_ecc_buf))<0) {
@@ -402,7 +457,94 @@ static int __init mtdecc_init(void)
 
 	mtd_ecc = mtd;
 
+
+  /* set-up BCH ECC */
+  /*
+	* examples:
+	* docg3.c:
+	*  	cascade->bch = init_bch(DOC_ECC_BCH_M,
+	*  		DOC_ECC_BCH_T,DOC_ECC_BCH_PRIMPOLY);
+	*
+	*  #define DOC_ECC_BCH_M                   14
+	*  #define DOC_ECC_BCH_T                   4
+	*  #define DOC_ECC_BCH_PRIMPOLY            0x4443
+	*
+	* docg4.c:
+	* 	doc->bch = init_bch(DOCG4_M, DOCG4_T, DOCG4_PRIMITIVE_POLY);
+	* 	#define DOCG4_M                14  // Galois field is of order 2^14 
+	*		#define DOCG4_T                4   // BCH alg corrects up to 4 bit errors
+	*		#define DOCG4_PRIMITIVE_POLY   0x4443
+	*
+	*	nand_bch.c:
+	*		nbc->bch = init_bch(m, t, 0);
+	*
+	*		m = fls(1+8*eccsize);
+	*		t = (eccbytes*8)/m;
+	*
+	*   from nand_base.c has 
+	*		eccsize = 512
+	*		eccbytes = 7
+	*
+	*		We are going to have 
+	*
+	*		eccsize = 3840
+	*		eccbytes = 255
+	*/
+#define MTD_ECC_BCH_M 15
+#define MTD_ECC_BCH_T 136
+	dev_notice(&mtd->dev,"Starting init_bch\n");
+	mtd_ecc_bch = init_bch(MTD_ECC_BCH_M,MTD_ECC_BCH_T,0);
+	if (!mtd_ecc_bch) {
+		printk(KERN_ERR "Error initializing BCH\n");
+    err = -ENOMEM;
+		goto init_free_mtd_ecc;
+	}
+	if (mtd_ecc_bch->ecc_bytes != 255) {
+		printk(KERN_ERR "Invalid eccbytes %u, should be 255\n",mtd_ecc_bch->ecc_bytes);
+    err = -EINVAL;
+		goto init_free_mtd_ecc_bch;
+	}
+  dev_notice(&mtd->dev,"ecc_bits: %d n:%d (n-ecc_bits:%d)\n",mtd_ecc_bch->ecc_bits,
+      mtd_ecc_bch->n,(mtd_ecc_bch->n-mtd_ecc_bch->ecc_bits));
+  {
+    unsigned char * erased_page;
+    mtd_ecc_eccmask = kmalloc(mtd_ecc_bch->ecc_bytes, GFP_KERNEL);
+    mtd_ecc_errloc = kmalloc(MTD_ECC_BCH_T*sizeof(unsigned int),GFP_KERNEL);
+    if (!mtd_ecc_eccmask || !mtd_ecc_errloc) {
+      printk(KERN_ERR "Unable to allocate %d bytes for mtd_ecc_eccmask\n",mtd_ecc_bch->ecc_bytes);
+      goto init_free_eccmask;
+    }
+    erased_page = kmalloc(3840,GFP_KERNEL);
+    if (!erased_page) {
+      printk(KERN_ERR "Unable to allocate erased_page of %d bytes\n",0xf00);
+      goto init_free_eccmask;
+    }
+    memset(erased_page,0xff,0xf00);
+    memset(mtd_ecc_eccmask,0,mtd_ecc_bch->ecc_bytes);
+    encode_bch(mtd_ecc_bch,erased_page,0xf00,mtd_ecc_eccmask);
+    kfree(erased_page);
+    for (i=0;i<mtd_ecc_bch->ecc_bytes;i++) {
+      mtd_ecc_eccmask[i] ^= 0xff;
+    }
+#if 0
+    dev_notice(&mtd->dev,"eccmask:\n");
+    for (i=0;i<0xff;i+=8) {
+      dev_notice(&mtd->dev,"%02x %02x %02x %02x %02x %02x %02x %02x\n",mtd_ecc_eccmask[i],
+        mtd_ecc_eccmask[i+1],mtd_ecc_eccmask[i+2],mtd_ecc_eccmask[i+3],
+mtd_ecc_eccmask[i+4],mtd_ecc_eccmask[i+5],mtd_ecc_eccmask[i+6],mtd_ecc_eccmask[i+7]);
+    }
+#endif
+  }
+  dev_notice(&mtd->dev,"Finished init_bch\n");
+
 	return 0;
+
+init_free_eccmask:
+  kfree(mtd_ecc_eccmask);
+  kfree(mtd_ecc_errloc);
+
+init_free_mtd_ecc_bch:
+	free_bch(mtd_ecc_bch);
 
 init_free_mtd_ecc:
 	kfree(mtd);
